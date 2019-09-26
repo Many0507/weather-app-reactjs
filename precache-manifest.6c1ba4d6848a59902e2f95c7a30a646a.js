642c13867c1139432ce5017d05d600ef672e1969
Manuel Lizama Paredes <lizama.paredes07@gmail.com>
2019-09-25 23:01:35 -0500

    Updates

---
+++ precache-manifest.6c1ba4d6848a59902e2f95c7a30a646a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a51d66c8e8cda834773eac4bdbabfbb",
+    "url": "/weather-app-reactjs/index.html"
+  },
+  {
+    "revision": "3ede3f16cae5427fb339",
+    "url": "/weather-app-reactjs/static/css/main.bf06d9b8.chunk.css"
+  },
+  {
+    "revision": "c8a7d184fc3ac870532f",
+    "url": "/weather-app-reactjs/static/js/2.c2e93aad.chunk.js"
+  },
+  {
+    "revision": "3ede3f16cae5427fb339",
+    "url": "/weather-app-reactjs/static/js/main.198aee0c.chunk.js"
+  },
+  {
+    "revision": "0846c30b9ea40f1572f7",
+    "url": "/weather-app-reactjs/static/js/runtime~main.625800b0.js"
+  },
+  {
+    "revision": "7cced4f2c5b626514227155e36720426",
+    "url": "/weather-app-reactjs/static/media/background.7cced4f2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Many0507/weather-app-reactjs/precache-manifest.9b0f731244ce4e89d0016c1f9e0e476c.js"
+  "/weather-app-reactjs/precache-manifest.6c1ba4d6848a59902e2f95c7a30a646a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Many0507/weather-app-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app-reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
