f262dbad3d1d833a2ebae0b1b932b57b3ba4f50e
Manuel Lizama Paredes <lizama.paredes07@gmail.com>
2019-09-25 22:46:58 -0500

    Updates

---
+++ precache-manifest.9b0f731244ce4e89d0016c1f9e0e476c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60181074c719111138ed92da41ebded",
+    "url": "/Many0507/weather-app-reactjs/index.html"
+  },
+  {
+    "revision": "5f79a1f80eb91d1e1880",
+    "url": "/Many0507/weather-app-reactjs/static/css/main.88b30523.chunk.css"
+  },
+  {
+    "revision": "c8a7d184fc3ac870532f",
+    "url": "/Many0507/weather-app-reactjs/static/js/2.c2e93aad.chunk.js"
+  },
+  {
+    "revision": "5f79a1f80eb91d1e1880",
+    "url": "/Many0507/weather-app-reactjs/static/js/main.198aee0c.chunk.js"
+  },
+  {
+    "revision": "ac1cf39b50a099543287",
+    "url": "/Many0507/weather-app-reactjs/static/js/runtime~main.60a9f0b6.js"
+  },
+  {
+    "revision": "7cced4f2c5b626514227155e36720426",
+    "url": "/Many0507/weather-app-reactjs/static/media/background.7cced4f2.jpg"
+  }
+]);
